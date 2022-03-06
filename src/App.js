b15bcb7287196eb7d82f433eb2a13abb214f85e1
Aldo Rojas <aldogrojas5@gmail.com>
2022-03-06 02:47:03 -0300

    layout added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import Splash from './components/Splash'
 
+import React, { useState } from 'react'
+import { Layout } from 'antd'
+
+import { MenuOutlined } from '@ant-design/icons'
+
+import CustomDrawer from './CustomDrawer'
+
+import logoHorizontal from './assets/images/logo-horizontal.svg'
 import './App.css'
 
+const { Header, Content } = Layout
+
 const App = () => {
+  const [collapsed, setCollapsed] = useState(false)
+
+  const toggleCollapsed = () => setCollapsed(!collapsed)
+
   return (
     <div className="App">
-      <Splash />
+      <CustomDrawer collapsed={collapsed} toggleCollapsed={toggleCollapsed} />
+      <Layout>
+        <Layout className="site-layout">
+          <Header className="site-layout-background layout-header" style={{ padding: 0 }}>
+            <MenuOutlined
+              className="trigger"
+              onClick={toggleCollapsed}
+            />
+            <img src={logoHorizontal} height="50px" alt="logo" /><b>Escuela de terapias complementarias</b>
+          </Header>
+          <Content
+            className="site-layout-background layout-content"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 'calc(100vh - 112px)',
+            }}
+          >
+            Contenido
+          </Content>
+        </Layout>
+      </Layout>
     </div>
   )
 }
